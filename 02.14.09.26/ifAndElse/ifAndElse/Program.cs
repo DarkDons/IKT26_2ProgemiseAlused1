cc0e52cc6c393e0bcf97703747b81cc0ac792eb6
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 10:18:32 +0300

    IfAndElse

---
+++ 02.14.09.26/ifAndElse/ifAndElse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ifAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ain-hanree!");
+
+            //siin on muutuja nimega name,
+            //mis tüübiga sting
+            //loeb andmeid konsoolist ja salvestab
+            //need muutuja name sisse
+            string name = Console.ReadLine();
+            
+            //if ja else kontrollib, kas muutuja
+            //name on tühi või mitte
+
+            //kui muutuja name on yühi, siis väljastab konsoolile
+            //teksti "tere, tundmatu! Error" ja teeb 4 piiksu
+            if (name != "")
+            {
+                Console.WriteLine("tere, " + name);
+            }
+            else
+            {
+                Console.WriteLine("tere, tundmatu! ERROR");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                  
+            }
+        }
+    }
+}
